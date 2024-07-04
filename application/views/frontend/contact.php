a98e2209feac8b5de54dd5946ccea5d436661e11
assystemth <dev@assystem.co.th>
2024-07-04 17:36:34 +0700

    contact

--- application/views/frontend/contact.php
+++ application/views/frontend/contact.php
@@ -1,4 +1,4 @@
-<div class="text-center pages-head" >
+<div class="text-center pages-head">
     <span class="font-pages-head">ติดต่อเรา</span>
 </div>
 <div class="text-center" style="padding-top: 50px">
@@ -6,23 +6,23 @@
 </div>
 </div>
 
-<div class="bg-pages" style="margin-top: 30px; margin-bottom: 20px;" >
+<div class="bg-pages" style="margin-top: 30px; margin-bottom: 20px;">
     <div class="container-pages-news">
         <div class="path text-center">
             <div class="mt-4">
-                <span class="font-contact-1">องค์การบริหารส่วนตำบลบ้านกลาง <br><br> เลขที่ 222 หมู่ที่ 10 ตำบลบ้านกลาง อำเภอหล่มสัก จังหวัดเพชรบูรณ์ 67110</span><br>
+                <span class="font-contact-1">องค์การบริหารส่วนตำบลซับพุทรา <br><br> เลขที่ 111 หมู่ 3 ถนนโคกยาว-บัววัฒนา ตำบลซับพุทรา อำเภอชนแดน จังหวัดเพชรบูรณ์ 67150</span><br>
                 <br>
                 <span class="font-contact-2">
-                    อีเมล : Ban_klang@hotmail.com <br><br>
-                    โทรศัพท์ : 056-910059 <br><br>
-                    Facebook : <a href="https://www.facebook.com/p/%E0%B8%AD%E0%B8%9A%E0%B8%95-%E0%B8%9A%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%81%E0%B8%A5%E0%B8%B2%E0%B8%87-%E0%B8%AD%E0%B8%B3%E0%B9%80%E0%B8%A0%E0%B8%AD%E0%B8%AB%E0%B8%A5%E0%B9%88%E0%B8%A1%E0%B8%AA%E0%B8%B1%E0%B8%81-%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B9%80%E0%B8%9E%E0%B8%8A%E0%B8%A3%E0%B8%9A%E0%B8%B9%E0%B8%A3%E0%B8%93%E0%B9%8C-100010841466180/?locale=th_TH" target="_blank" rel="noopener noreferrer">www.facebook.com/องค์การบริหารส่วนตำบลบ้านกลาง</a>
+                    <!-- อีเมล : Ban_klang@hotmail.com <br><br> -->
+                    โทรศัพท์ : 056-713-895 <br><br>
+                    Facebook : <a href="https://www.facebook.com/people/%E0%B8%AD%E0%B8%87%E0%B8%84%E0%B9%8C%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%9A%E0%B8%A3%E0%B8%B4%E0%B8%AB%E0%B8%B2%E0%B8%A3%E0%B8%AA%E0%B9%88%E0%B8%A7%E0%B8%99%E0%B8%95%E0%B8%B3%E0%B8%9A%E0%B8%A5%E0%B8%8B%E0%B8%B1%E0%B8%9A%E0%B8%9E%E0%B8%B8%E0%B8%97%E0%B8%A3%E0%B8%B2/100064917631107/" target="_blank" rel="noopener noreferrer">www.facebook.com/องค์การบริหารส่วนตำบลซับพุทรา</a>
                 </span>
             </div>
             <div class="mt-5 mb-3">
                 <span class="font-contact-map">แผนที่หน่วยงาน</span>
             </div>
             <!-- <iframe class="map-contact" width="873" height="594" frameborder="0" src="https://www.google.com/maps?q=องค์การบริหารส่วนตำบลสว่าง&output=embed" allowfullscreen></iframe> -->
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3823.239828980854!2d101.25746457514768!3d16.614728484146667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31204bb6a5cc0671%3A0x26bd823d1208dd4a!2z4Lit4LiH4LiE4LmM4LiB4Liy4Lij4Lia4Lij4Li04Lir4Liy4Lij4Liq4LmI4Lin4LiZ4LiV4Liz4Lia4Lil4Lia4LmJ4Liy4LiZ4LiB4Lil4Liy4LiHIOC4rS7guKvguKXguYjguKHguKrguLHguIEg4LiILuC5gOC4nuC4iuC4o-C4muC4ueC4o-C4k-C5jA!5e0!3m2!1sen!2sth!4v1718774749557!5m2!1sen!2sth" width="873" height="595" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15336.845391029829!2d100.9193218!3d16.0545188!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311fecfe13b315ed%3A0x1df075d92a21ffb4!2z4Lit4LiH4LiE4LmM4LiB4Liy4Lij4Lia4Lij4Li04Lir4Liy4Lij4Liq4LmI4Lin4LiZ4LiV4Liz4Lia4Lil4LiL4Lix4Lia4Lie4Li44LiX4Lij4Liy!5e0!3m2!1sen!2sth!4v1720089286825!5m2!1sen!2sth" width="873" height="595" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 </div>
